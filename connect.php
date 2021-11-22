0d8fff1f856db4c880e3cc4bc9b0e07a6143e6b3
Veron Tan <61444629+veronwltan@users.noreply.github.com>
2021-11-22 21:41:27 +0800

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,38 +1,81 @@
 
 
 <?php
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "test";
-
 $offer = $_POST['eth69'] . ' ETH';
-$usd = '$' . $offer * 4000;
-$percent = (1 - ($offer /7.33)) * 100;
-$roundpercent = round($percent,2) . '% Below';
+$gay = 0;
+$gaynon = 1;
 
 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "CREATE TABLE ethoffer (
+  Price VARCHAR(500),
+  USD VARCHAR(500),
+  Superidol VARCHAR(500),
+  Expiration VARCHAR(500),
+  Previous VARCHAR(500),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+
+if($offer >= 7.33){
+  ?>
+  <html>
+  <form id="form" action="itempage2.php" method="POST">
+  <input type="hidden" name="gay" value="<?php echo $gaynon ?>">
+  </form>
+  <script>
+  document.getElementById("form").submit();
+  </script>
+  </html>
+  <?php
+  echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+  }
+
+
+
 if($offer < 7.33){
-    $sql = "INSERT INTO ethoffer (Price,USD,Superidol,Expiration,Previous) VALUES ('$offer','$usd','$roundpercent','in 20 hours','ChadSquid')";
-    header("Location:./itempage2.php");
-}else if($offer >= 7.33){
-    $sql = "INSERT INTO ethoffer (Price,USD,Superidol,Expiration,Previous) VALUES ('$offer','$usd','$roundpercent','in 20 hours in your dick','ChadSquid')";
-    header("Location:./itempage2.php");
+    ?>
+<html>
+<form id="form" action="itempage2.php" method="POST">
+<input type="hidden" name="gay" value="<?php echo $gay ?>">
+</form>
+</form>
+<script>
+document.getElementById("form").submit();
+</script>
+</html>
+<?php 
 }
+
+
+if($offer < 7.33){
+  $usd = '$' . $offer * 4000;
+  $percent = (1 - ($offer /7.33)) * 100;
+  $roundpercent = round($percent,2) . '% Below';
+  $sql = "INSERT INTO ethoffer (Price,USD,Superidol,Expiration,Previous) VALUES ('$offer','$usd','$roundpercent','in 20 hours','ChadSquid')";
+  header("Location:./itempage2.php");
+}  
+
 
 
 if ($conn->query($sql) === TRUE) {
   echo $offer;
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
